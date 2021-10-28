4dadb42ba5e887fad7585e6da54a2c5cf5dded72
rajthilakam <rajthilakam@gmail.com>
2021-10-27 18:08:11 -0700

    Length of Longest substring

---
+++ Strings/Length_Longest_Substring.py
@@ -0,0 +1,28 @@
+def LCS(X, Y, m, n):
+ 
+    maxLength = 0           
+    endingIndex = m         
+     
+    lookup = [[0 for x in range(n + 1)] for y in range(m + 1)]
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1): 
+            
+            if X[i - 1] == Y[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1] + 1
+                 
+                if lookup[i][j] > maxLength:
+                    maxLength = lookup[i][j]
+                    endingIndex = i
+
+    return maxLength
+    #return X[endingIndex - maxLength: endingIndex]
+  
+X = 'ABC'
+Y = 'BABA'
+
+m = len(X)
+n = len(Y)
+
+print('The lenght of longest common substring is', LCS(X, Y, m, n))
+ 
