d39f3f5ee8b9be95710a62fad68af7466db060b2
rajthilakam <rajthilakam@gmail.com>
2021-10-29 15:01:14 -0700

    Recursively_remove_duplicate

---
+++ Strings/Recursive_remove_duplicates.py
@@ -0,0 +1,26 @@
+def str_remove(string1):
+    res = string1
+    temp = ''
+    while (len(temp) != len(res)):
+        temp = res
+        res = remove_adj(res)
+        
+    return res
+    
+def remove_adj(s):
+    
+    i = 0
+    n = len(s)
+    res = ''
+    while(i < n):
+        if (i < n-1 and s[i] == s[i+1]):
+            while (i < n-1 and s[i] == s[i+1]):
+                i+=1
+                
+        else:
+            res+=s[i]
+        i+=1
+    
+    return res
+        
+str_remove('acaaabbbacdddd') 
