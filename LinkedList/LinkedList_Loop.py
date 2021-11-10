5af8c8544f020e261add19c38a265d913aca1d97
rajthilakam <rajthilakam@gmail.com>
2021-11-10 15:34:28 -0800

    Find Loop in a Lnkedlist

---
+++ LinkedList/LinkedList_Loop.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None  
+        
+    def traverse_list(self):
+        if self.head is None:
+            print("List has no element")
+            return
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data , " ")
+                n = n.next  
+
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def findLoop(self):
+        slow = self.head
+        fast = self.head
+        while(slow and fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False           
+
+
+ll = LinkedList()        
