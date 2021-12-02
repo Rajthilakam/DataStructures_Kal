4d46d880777a4ac7d8c9c4c1acbb2ef234dd8bed
rajthilakam <rajthilakam@gmail.com>
2021-12-01 20:19:48 -0800

    Two Sum using Hashmap

---
+++ Arrays/Two_sum_extraspace.py
@@ -0,0 +1,19 @@
+#Time Complexity 0(n) Space complexity o(n)
+
+
+a = [-3,2,3,3,6,8,15]
+
+target = 6
+
+
+def two_sum_hash(a,target):
+    hashed = {}
+    for i,n in enumerate(a):
+        diff = target - n
+        if diff in hashed:
+            print([hashed[diff]+1,i+1])
+            return ([hashed[diff],i])
+        hashed[n] = i    
+
+
+two_sum_hash(a,target)
