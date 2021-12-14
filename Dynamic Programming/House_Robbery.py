48f994f4951c52d06154d12cc1c5af81ce8c7433
rajthilakam <rajthilakam@gmail.com>
2021-12-13 22:58:30 -0800

    House Robber

---
+++ Dynamic Programming/House_Robbery.py
@@ -0,0 +1,20 @@
+
+def house_robber(nums):
+    if not nums: 
+        return 0
+    if len(nums) == 1: 
+        return nums[0]
+    
+    dp = [0] * len(nums)
+
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+      dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+    print(dp[-1])  
+    return dp[-1] # return the last element
+
+nums = [1,2,3,4]    
+house_robber(nums)
