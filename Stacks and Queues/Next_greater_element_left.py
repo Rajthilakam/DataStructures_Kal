4faa01501e2c51c427ff83f07a8165c3c280a5b0
rajthilakam <rajthilakam@gmail.com>
2021-12-16 16:01:58 -0800

    Next greatest element to left

---
+++ Stacks and Queues/Next_greater_element_left.py
@@ -0,0 +1,17 @@
+input = [2, 7, 3,9, 4, 6, 5]
+
+def findgreaterleft(input):
+    if not input:
+        return 
+    s = []
+    result = [-1] * len(input)
+
+    for i in reversed(range(len(input))):
+        while s and input[i] > input[s[-1]]:
+            result[s[-1]] = input[i]
+            s.pop()
+        s.append(i)
+    print(result)         
+    return result    
+
+findgreaterleft(input)
