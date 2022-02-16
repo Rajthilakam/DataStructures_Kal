eef3395aeaa1ff3f9e0e1e32dd3cfded8c128fab
rajthilakam <rajthilakam@gmail.com>
2022-02-16 10:43:21 -0800

    Reverse a string within paranthesis

---
+++ Strings/Reverse_String_para.py
@@ -0,0 +1,25 @@
+#Reverse string without paranthesis
+
+def solution(s):
+    opened = []
+    pair = {}
+    for i, c in enumerate(s):
+            if c == '(':
+                opened.append(i)
+            if c == ')':
+                j = opened.pop()
+                pair[i], pair[j] = j, i
+    res = []
+    i, d = 0, 1
+    while i < len(s):
+        if s[i] in '()':
+            i = pair[i]
+            d = -d
+        else:
+            res.append(s[i])
+        i += d
+    print(''.join(res))    
+    return ''.join(res)
+
+s = '(ab(for)cd)'
+solution(s)
